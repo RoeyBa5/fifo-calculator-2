61babe1ab5675e56e9374d3cb2a2366fa85b8003
roeybarda <106654332+roeybarda@users.noreply.github.com>
2022-11-20 00:42:45 +0200

    inbit

--- fastapi_setup.sh
+++ fastapi_setup.sh
@@ -9,7 +9,7 @@ cd ~/AWS-EC2-Setup-for-FastAPI-and-Ngrok-Deployment
 # Install FastAPI application requirements
 pip3 install -r requirements.txt
 # Kill any service running on port 80
-sudo kill -9 $(sudo lsof -t -i:80)
+sudo kill -9 $(sudo lsof -t -i:8000)
 sudo service nginx restart
 # Run the application with nohup so the application runs as a background process
 nohup python3 -m uvicorn app:app --reload --host 0.0.0.0
